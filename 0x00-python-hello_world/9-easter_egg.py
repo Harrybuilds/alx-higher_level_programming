9e2cee7f5183b7710c4fd995ff5f3279c2a1e25f
Harrison <harrisonchialuka@gmail.com>
2023-08-09 16:20:33 +0000

    task 9

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import this
 
-# Adding comment to comlete this file 98 characters as requested
+# Addcomment to comlete this file 98 characters as requested
