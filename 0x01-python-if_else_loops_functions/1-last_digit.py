19d9e6bfaa17dbe9d08df5068e83161a6c191471
Harrison <harrisonchialuka@gmail.com>
2023-08-09 19:03:44 +0000

    python if else project

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,10 +10,10 @@ greater_than_5 = "and is greater than 5"
 zero = "and is 0"
 less_than_6_not_0 = "and is less than 6 and not 0"
 
-if int(last_digit) == 0:
-    print(f"{must_print} {zero}")
-
-elif int(last_digit) > 5:
+if int(last_digit) > 5:
     print(f"{must_print} {greater_than_5}")
+
+elif int(last_digit) == 0:
+    print(f"{must_print} {zero}")
 else:
     print(f"{must_print} {less_than_6_not_0}")
