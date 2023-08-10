0eb611169c869cdfcc939c43fc7177993e2aa5bd
Harrison <harrisonchialuka@gmail.com>
2023-08-10 17:38:51 +0000

    task 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,4 @@ add = ad.add
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ import calculator_1 as cal
 a = 10
 b = 5
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, cal.add(a, b)))
-    print("{} + {} = {}".format(a, b, cal.sub(a, b)))
-    print("{} + {} = {}".format(a, b, cal.mul(a, b)))
-    print("{} + {} = {}".format(a, b, cal.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, cal.sub(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, cal.mul(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, cal.div(a, b)))
