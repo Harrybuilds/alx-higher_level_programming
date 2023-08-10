34c973a6725fa49bbf65cfd4c38acf3edf165db4
Harrison <harrisonchialuka@gmail.com>
2023-08-10 02:21:33 +0000

    task 12 done at 3:18am

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
 
     while i != 100:
         if i % 15 == 0:
-            print("{}".format("FIZZ BUZZ"), end=' ')
+            print("{}".format("FizzBuzz"), end=' ')
         elif i % 5 == 0:
             print("{}".format("BUZZ"), end=' ')
         elif i % 3 == 0:
