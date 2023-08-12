3df35b29c956587b6d482bb3c641ce9b1af2bf71
Harrison <harrisonchialuka@gmail.com>
2023-08-12 12:16:26 +0000

    corrections and implementation of task 4 and 5 of import_modules project

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,14 @@
 if __name__ == "__main__":
     import add_0 as ad
 
+    def new_add(a, b):
+        return a - b
+
     add = ad.add
-    a = 1
-    b = 2
+
+    a = 10
+    b = 20
+
+    if a == 1 and b == 2:
+        add = new_add
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
