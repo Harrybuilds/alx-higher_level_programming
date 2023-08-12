a3fdec0c690e682b02b361241a1b48aec5f81792
Harrison <harrisonchialuka@gmail.com>
2023-08-12 14:51:42 +0000

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/ython3
+
+def element_at(my_list, idx):
+    i = 0
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+
+    for i in range(len(my_list)):
+        if i == idx:
+            return my_list[idx]
