a925e809ecd1eed53fd2bf1adc72977b66748b11
Harrison <harrisonchialuka@gmail.com>
2023-08-13 23:17:34 +0000

    task 8 correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        first = None
     length = len(sentence)
     first = sentence[0]
+
+    if len(sentence) == 0:
+        first = None
 
     return length, first
