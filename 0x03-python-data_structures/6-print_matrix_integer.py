fea8cd4bd684883a966784c39bb648958d91a364
Harrison <harrisonchialuka@gmail.com>
2023-08-14 16:17:00 +0000

    task 6 showing me shege

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        x = " ".join(map(lambda x: "{}".format(x), matrix[i]))
-        print("{}".format(x))
+        #        x = " ".join(map(lambda x: "{}".format(x), matrix[i]))
+        x = matrix[i]
+        #       xprint("{:d}".format(*x))
+        print(*x)
