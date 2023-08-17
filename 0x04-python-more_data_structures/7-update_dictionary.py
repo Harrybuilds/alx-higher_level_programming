28d3cb0374087c7fd7d38faf3deafa2072f05275
Harrison <harrisonchialuka@gmail.com>
2023-08-17 13:48:03 +0000

    pushing task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    return a_dictionary[key] = value
+    To_be_updated = {key:value}
+    return a_dictionary.update(To_be_updated)
