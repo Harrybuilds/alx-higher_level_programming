a18a199656a85aaf10858adc8da2b1476108c4fa
Harrison <harrisonchialuka@gmail.com>
2023-08-17 15:20:58 +0000

    task 6 correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,8 +2,10 @@
 
 def print_sorted_dictionary(a_dictionary):
     new_list = sorted(a_dictionary.items(),
-                      key=lambda x: x)
+                      key=lambda x:x)
+    #new_list is now a list of tuples(key,value)
+
     new_dict = dict(new_list)
 
-    for k, v in new_dict.items():
-        print("{:s} {}".format(k, v))
+    for k,v in new_dict.items():
+        print("{:s}: {}".format(k, v))
