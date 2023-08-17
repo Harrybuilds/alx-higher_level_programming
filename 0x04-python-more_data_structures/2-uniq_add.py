bad2e5cdfeea8ca13f6eaf5adf8cf19489e95b2a
Harrison <harrisonchialuka@gmail.com>
2023-08-17 12:30:29 +0000

    make corrections on task 2 and 4

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -9,15 +9,14 @@ def uniq_add(my_list=[]):
         use list comprehension to
         append unique numbers to
         num_tracker list
-        
+
         option2. typecast list to a set
         the sum up all values
         """
         [num_tracker.append(i) for i in
-               my_list if i not in num_tracker]
+         my_list if i not in num_tracker]
 
         for i in num_tracker:
             total += i
 
         return total
-    
