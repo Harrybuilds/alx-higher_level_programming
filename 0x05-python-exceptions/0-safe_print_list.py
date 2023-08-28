b17942863f378af67516bdf0dac8aa8dad700cdb
Harrison <harrisonchialuka@gmail.com>
2023-08-28 10:08:04 +0000

    task 0 of python error handling project: Try,except, raise

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        new_list = my_list[0 : x]
-        for element in new_list:
+    if my_list is None:
+        return
+    new_list = my_list[0:x]
+    for element in new_list:
+        try:
             print("{:d}".format(element), end="")
             if element is new_list[-1]:
                 print()
-        last_digit = new_list[-1]
-        return last_digit
-    except ValueError:
-        return
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    last_digit = new_list[-1]
+    return last_digit
