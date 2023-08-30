62e7da68c047a14e0ebe650af5d54b2ef23abb6f
Harrison <harrisonchialuka@gmail.com>
2023-08-30 18:46:06 +0000

    task 0-safe_print_list.py on error handling in python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             if element is new_list[-1]:
                 print()
         except (ValueError, TypeError, IndexError):
-            pass
+            break
     last_digit = new_list[-1]
     return last_digit
