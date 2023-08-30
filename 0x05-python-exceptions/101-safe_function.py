9f74e8e6b58157b966683471104ed94409f2d576
Harrison <harrisonchialuka@gmail.com>
2023-08-30 22:41:13 +0000

    task 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,4 +8,5 @@ def safe_function(fct, *args):
         a, b = args[0], args[1]
         return fct(a, b)
     except (ZeroDivisionError, IndexError, NameError) as e:
-        print("Exception:", e, file=sys.stderr)
+        print("{} {} ".format("Exception:", e), file=sys.stderr)
+        return None
