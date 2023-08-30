815191383fc4ff060ca169524afa2dcd001ead76
Harrison <harrisonchialuka@gmail.com>
2023-08-30 22:25:37 +0000

    task 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        a, b = args[0], args[1]
+        return fct(a, b)
+    except ZeroDivisionError as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
+    except IndexError as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
