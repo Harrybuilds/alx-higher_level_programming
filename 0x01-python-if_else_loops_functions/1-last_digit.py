898d27721a198af771d1ec7cc1a7b1ea9a547609
Harrison <harrisonchialuka@gmail.com>
2024-01-17 02:01:18 +0100

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,11 @@ import random
 allmsg = ["and is greater than 5", "and is 0", "and is less than 6 and not 0"]
 
 number = random.randint(-10000, 10000)
-last_digit = int(f"{str(number)[-1]}") if number > 0 else int(f"{str(number)[-1]}") * -1
+if number > 0:
+    last_digits = int(f"{str(number)[-1]}")
+else:
+    last_digit = int(f"{str(number)[-1]}") * -1
+
 if last_digit > 5:
     msg = allmsg[0]
 elif last_digit == 0:
