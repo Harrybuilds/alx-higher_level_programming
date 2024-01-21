b51c77848e808983c164ae70ccf3de123912d435
Harrison <harrisonchialuka@gmail.com>
2024-01-21 19:01:39 +0100

    correction on task 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,4 +5,10 @@ def max_integer(my_list=[]):
     without using the built-in max()
     """
 
-    return eval(f"max({my_list}) if len(my_list) > 0 else None")
+    if len(my_list) == 0:
+        return None
+    max_num = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    return max_num
