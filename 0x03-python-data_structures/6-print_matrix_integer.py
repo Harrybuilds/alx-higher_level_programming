a3085ad068a9cb8be46919067d0bc26f12f9793d
Harrison <harrisonchialuka@gmail.com>
2024-01-21 20:01:59 +0100

    correction on task 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,6 @@ def print_matrix_integer(matrix=[[]]):
 
     for r in (matrix):
         for c in r:
-            print("{:d}".format(c), end=" " if r.index(c) != len(r) - 1 else "")
+            end = " " if r.index(c) != len(r) - 1 else ""
+            print("{:d}".format(c), end=end)
         print()
