88fa4f86a1baf6c9e500c94e55240c927a8b3945
Harrison <harrisonchialuka@gmail.com>
2024-01-23 21:15:11 +0100

    correction on task 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,26 +2,30 @@
 
 if __name__ == "__main__":
     import sys
-    import calculator_1 as cal
+    import calculator_1 as c
 
     if len(sys.argv[1:]) != 3:
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         sys.exit(1)
 
     operations = ["+", "-", "*", "/"]
-    
+
     if sys.argv[2] in operations:
 
         operation = operations.index(sys.argv[2])
 
+        a1 = int(sys.argv[1])
+        a2 = sys.argv[2]
+        a3 = int(sys.argv[3])
+
         if operation == 0:
-            print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {cal.add(int(sys.argv[1]), int(sys.argv[3]))}")
+            print("{} {} {} = {}".format(a1, a2, a3, c.add(a1, a3)))
         elif operation == 1:
-             print("{} {} {} = {}".format(sys.argv[1],sys.argv[2],sys.argv[3],cal.sub(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} {} {} = {}".format(a1, a2, a3, c.sub(a1, a3)))
         elif operation == 2:
-             print("{} {} {} = {}".format(sys.argv[1],sys.argv[2],sys.argv[3],cal.mul(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} {} {} = {}".format(a1, a2, a3, c.mul(a1, a3)))
         else:
-             print("{} {} {} = {}".format(sys.argv[1],sys.argv[2],sys.argv[3],cal.div(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} {} {} = {}".format(a1, a2, a3, c.div(a1, a3)))
     else:
         print(f"Unknown operator. Available operators: +, -, * and / ")
         sys.exit(1)
