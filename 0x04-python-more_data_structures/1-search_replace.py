3d2a788e6b1c30fd0031037d29c6540b55733c03
Harrison <harrisonchialuka@gmail.com>
2024-01-24 12:30:28 +0100

    correction on task 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
     # my_list: is the initial list
     # search: is the element to replace in the list
     # replace: is the new element
 
-    new_list = [i for i in my_list]
+    new_list = [*my_list]
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
