24a15c65544fdec35ff5b614df1c3958dd6289a7
Harrison <harrisonchialuka@gmail.com>
2024-02-06 18:42:10 +0100

    third correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+d#!/usr/bin/python3
 
 class Square:
     """ a class that defines a square """
 
     def __init__(self, size=0):
         """ instantiates every object of this square class """
-        self.__size = size
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,11 +5,11 @@ class Square:
 
     def __init__(self, size=0):
         """ instantiates every object of this square class """
-        self.__size = size
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """ computes and returns the area of the square object """
