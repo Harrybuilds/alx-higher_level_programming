4bbe704c0ae753089b6ffaf4ff7375228810f33c
Harrison <harrisonchialuka@gmail.com>
2024-02-06 18:07:10 +0100

    classes and object oriented programming

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """ a class that defines a square """
+
+    def __init__(self, size):
+        """ instantiates every object of this square class """
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """ a class that defines a square """
+
+    def __init__(self, size=0):
+        """ instantiates every object of this square class """
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ computes and returns the area of the square object """
+        return self.__size **2
