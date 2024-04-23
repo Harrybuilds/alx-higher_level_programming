71157d5d1fedbb80ae1bbddb61e28da3b953178a
Harrison <harrisonchialuka@gmail.com>
2024-04-23 07:08:46 +0100

    now executable right permited files

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const recur = (num) => {
+    num = parseInt(num);
+    if (isNaN(num)){
+	return 1;
+    }
+    if (num <= 1){
+	return num;
+    }
+    return num + recur(num - 1);
+}
+
+console.log(recur(process.argv[2]));
