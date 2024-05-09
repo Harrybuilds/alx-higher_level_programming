cf8070df1911e88fe3b07fd3ef7dcbdc164bb2b0
Harrison <harrisonchialuka@gmail.com>
2024-05-09 22:21:38 +0100

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# function to findthepeak number in a list
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    mid = len(list_of_integers) // 2
+    if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        return list_of_integers[mid]
+    elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
