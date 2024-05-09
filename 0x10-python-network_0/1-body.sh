d9d6d139a1bb5113c498e5730198c3c2cf97986b
Harrison <harrisonchialuka@gmail.com>
2024-05-09 19:53:31 +0100

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #using curl, display content of url if status == 200
-#res=$(curl -sI -X GET "$1")
-res=$(curl -sI "$1" | grep "HTTP" | cut -d ' ' -f 2)
-if [ "$res" == 200 ]; then
-    curl -sX GET "$1"
-fi
+curl -sLX GET "$1"
