2cad10805121259c9cd0370b8542b4fba3030405
Harrison <harrisonchialuka@gmail.com>
2024-05-09 19:44:54 +0100

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 #Bash script to display size of given url in bytes
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request using curl and get the size of the response body
-#size=$(curl -s -w "%{size_download}" -o /dev/null "$1")
 curl -sI "$1" | grep "content-length" | cut " " -f 2
-echo "$size"
-
