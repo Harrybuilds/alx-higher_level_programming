5ffb8d2f235fa198528640a5cd1b14bb05ac676d
Harrison <harrisonchialuka@gmail.com>
2024-06-05 23:18:52 +0100

    advance task 1: 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,35 +1,35 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
 const getNames = (movieId) => {
-    const options = {
-	uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
-	method: "GET",
-	json: true
+  const options = {
+    uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+    method: 'GET',
+    json: true
+  };
+
+  request(options, (err, res, body) => {
+    if (err) {
+      console.error(err);
     }
+    const xter = body.characters;
+    for (let i = 0; i < xter.length; i++) {
+      const req = {
+        uri: xter[i],
+        method: 'GET',
+        json: true
+      };
+      request(req, (err, res, body) => {
+        if (err) {
+          console.error(err);
+        }
+        console.log(body.name);
+      });
+    }
+  });
+};
 
-    request(options, (err, res, body)=>{
-	if (err){
-	    console.error(err);
-	}
-	const xter = body.characters
-	for (let i=0; i < xter.length; i++){
-	    const req = {
-		uri: xter[i],
-		method: 'GET',
-		json: true
-	    }
-	    request(req, (err, res, body) => {
-		if (err){
-		    console.error(err);
-		}
-		console.log(body.name)
-	    });
-	}
-    })
-}
-
-const id = process.argv[2]
+const id = process.argv[2];
 
 getNames(id);
