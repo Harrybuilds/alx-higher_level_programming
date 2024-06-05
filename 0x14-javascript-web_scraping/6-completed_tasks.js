6dead55e6299e3f374b59ed61b0ccd3d13bf5253
Harrison <harrisonchialuka@gmail.com>
2024-06-05 20:33:58 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -17,12 +17,12 @@ const numTask = (url) => {
     }
     for (let k = 0; k < body.length; k++) {
       const task = body[k];
-      if (body[k].completed) {
-        if (task.userId in taskCount) {
-          taskCount[body[k].userId]++;
-        } else {
-          taskCount[body[k].userId] = 1;
-        }
+      if (task.completed) {
+          if (task.userId in taskCount) {
+              taskCount[task.userId]++;
+          } else {
+              taskCount[task.userId] = 1;
+          }
       }
     }
     console.log(taskCount);
