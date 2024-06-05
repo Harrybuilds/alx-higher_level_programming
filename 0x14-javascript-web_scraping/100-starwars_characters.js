e6e5fdfd018213fd483e395adc13807ce4ed680d
Harrison <harrisonchialuka@gmail.com>
2024-06-05 23:13:17 +0100

    advance task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,6 +4,32 @@ const request = require('request')
 
 const getNames = (movieId) => {
     const options = {
-	uri: 
+	uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+	method: "GET",
+	json: true
     }
+
+    request(options, (err, res, body)=>{
+	if (err){
+	    console.error(err);
+	}
+	const xter = body.characters
+	for (let i=0; i < xter.length; i++){
+	    const req = {
+		uri: xter[i],
+		method: 'GET',
+		json: true
+	    }
+	    request(req, (err, res, body) => {
+		if (err){
+		    console.error(err);
+		}
+		console.log(body.name)
+	    });
+	}
+    })
 }
+
+const id = process.argv[2]
+
+getNames(id);
